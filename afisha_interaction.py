1358f905ba15ae2f1f91d28b96637ee2cf49a83c
rndviktor2devman <7irb291wqkmu@mail.ru>
2017-01-18 00:34:51 +0300

    parsing movie data

--- afisha_interaction.py
+++ afisha_interaction.py
@@ -1,5 +1,9 @@
 import requests
+import re
+import json
 from bs4 import BeautifulSoup as BS
+
+NO_INFO = 'Информация отсутствует'
 
 
 def fetch_afisha_page():
@@ -7,18 +11,27 @@ def fetch_afisha_page():
     return requests.get(url).content
 
 
-def parse_afisha_data(raw_data):
-    soup = BS(raw_data, "lxml")
-    movies = []
-    for item in soup.find_all('div',
-                              "object s-votes-hover-area collapsed"):
-        movie = {}
-        a = item.find('h3', "usetags")
-        ref = a.find('a', href=True)
-        movie["title"] = a.text
-        movie["afishaUrl"] = "http:{}".format(ref['href'])
-        table = item.find('table')
-        movie["cinemas"] = len(table.find_all('tr'))
-        movies.append(movie)
+def movie_refs(base_page):
+    soup = BS(base_page, 'lxml')
+    movie_tags = soup.findAll(string=re.compile(r'\w+'), href=re.compile(r'movie/[0-9]+'))
+    return ['http:' + tag['href'] for tag in movie_tags]
 
-    return movies
+
+def parse_movie_data(raw_data):
+    movie = {}
+    soup = BS(raw_data, "lxml")
+    raw_data = json.loads(soup.find('script', {'type': 'application/ld+json'}).text)
+    movie['director'] = raw_data.get('director', {'name': NO_INFO})['name']
+    movie['text'] = raw_data.get('text', NO_INFO)
+    movie['description'] = raw_data.get('description', NO_INFO)
+    movie['genre'] = raw_data.get('genre')
+    format_duration = raw_data.get('duration', {'name': NO_INFO})['name']
+    movie['duration'] = re.sub(r'PT(\d+)H(\d+M)',
+                               lambda t: str(int(t.group(1)) * 60 +
+                                             int(t.group(2))), format_duration)
+    movie['release'] = re.sub(r'(\d{4}).*', r'\1', raw_data['datePublished'])
+    movie['voted'] = raw_data.get('aggregateRating', {'ratingCount': 0})['ratingCount']
+    movie['rating'] = raw_data.get('aggregateRating', {'ratingValue': 0})['ratingValue']
+    movie['url'] = raw_data.get('url')
+    movie['image'] = raw_data.get('image')
+    return movie
--- server.py
+++ server.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template
+import requests
+from afisha_interaction import fetch_afisha_page
+from werkzeug.contrib.cache import SimpleCache
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+cache = SimpleCache()
+
+
+def retrieve_cached(url, timeout=43200):
+    cached_page = cache.get(url)
+    if cached_page:
+        return cached_page
+    page = requests.get(url)
+    cache.add(url, page, timeout=timeout)
+    return page
 
 
 @app.route('/')
@@ -8,4 +22,5 @@ def films_list():
     return render_template('films_list.html')
 
 if __name__ == "__main__":
+    raw_data = fetch_afisha_page()
     app.run()
