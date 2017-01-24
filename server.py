080d58ca7c56e2e9be750ac9bd5e0b37ec52877e
rndviktor2devman <7irb291wqkmu@mail.ru>
2017-01-24 12:21:26 +0300

    retrieve site info

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 import requests
-from afisha_interaction import fetch_afisha_page
+from afisha_interaction import fetch_afisha_page, movie_refs, parse_movie_data
 from werkzeug.contrib.cache import SimpleCache
 
 app = Flask(__name__)
@@ -19,8 +19,13 @@ def retrieve_cached(url, timeout=43200):
 
 @app.route('/')
 def films_list():
-    return render_template('films_list.html')
+    page = fetch_afisha_page()
+    refs = movie_refs(page)
+    movies_data = []
+    print('found %d' % len(refs))
+    for ref in refs:
+        movies_data.append(parse_movie_data(requests.get(ref).content))
+    return render_template('films_list.html', movies=movies_data)
 
 if __name__ == "__main__":
-    raw_data = fetch_afisha_page()
     app.run()
