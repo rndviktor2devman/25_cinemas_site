2d7edb1cb2218cdb190ca2794ae5881201d318cb
rndviktor2devman <7irb291wqkmu@mail.ru>
2017-02-09 00:13:34 +0300

    polling turned off

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 var socket = io.connect('http://' + document.domain + ':' + location.port);
-    socket.on('connect', function() {
-        socket.emit('my event', {data: 'I\'m connected!'});
-    });
+    // socket.on('connect', function() {
+    //     socket.emit('my event', {data: 'I\'m connected!'});
+    // });
     socket.on('movie_loaded', function (movie) {
         console.log(movie);
         var d = document.getElementById('movies_row');
