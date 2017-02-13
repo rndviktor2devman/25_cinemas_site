0fc21e75bf5a6e6a706a264027f037dc9c44d312
rndviktor2devman <7irb291wqkmu@mail.ru>
2017-02-13 23:52:26 +0300

    loading made async

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,81 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var socket = io.connect('http://' + document.domain + ':' + location.port);
-    // socket.on('connect', function() {
-    //     socket.emit('my event', {data: 'I\'m connected!'});
-    // });
-    socket.on('movie_loaded', function (movie) {
-        console.log(movie);
-        var d = document.getElementById('movies_row');
-        var stub = document.getElementById('stub');
-        if(stub){
-            d.removeChild(stub);
-        }
-    });
+var MoviesList = React.createClass({displayName: "MoviesList",
+    getInitialState() {
+        return {
+            movies: [],
+            showSpinner: false
+        };
+    },
+
+    componentDidMount(){
+        socket.on('init', this._initialize);
+        socket.on('movie_loaded', this._load_movie);
+    },
+
+    _initialize(data){
+        var {movies, show} = data;
+        this.setState({movies, showSpinner: show});
+    },
+
+    _load_movie(movie){
+        var movies = this.state.movies;
+        movies.push(movie.data);
+        this.setState({movies, showSpinner: true});
+    },
+
+    render() {
+        var movies = this.state.movies;
+        var showSpinner = this.state.showSpinner;
+        return(
+            React.createElement("div", null, 
+                React.createElement("div", {className: "row"}, 
+                    React.createElement("div", {className: showSpinner ? 'loader' : 'loader hidden'})
+                ), 
+                React.createElement("ul", null, 
+                     movies.map(function (movie) {
+                        return React.createElement("li", null, 
+                            React.createElement("div", {className: "movie_class col-md-12 row"}, 
+                                React.createElement("div", {className: "col-md-4"}, 
+                                    React.createElement("div", {className: "moviediv"}, 
+                                        React.createElement("a", {href:  movie.url}, 
+                                            React.createElement("img", {src:  movie.image, className: "img-rounded movie_image", alt:  movie.title})
+                                        )
+                                    ), 
+                                    React.createElement("ul", null, 
+                                        React.createElement("li", null, React.createElement("b", null, "Режиссер: "),  movie.director), 
+                                        React.createElement("li", null, React.createElement("b", null, "Продолжительность: "),  movie.duration), 
+                                        React.createElement("li", null, React.createElement("b", null, "Дата релиза: "),  movie.release)
+                                    )
+                                ), 
+                                React.createElement("div", {className: "col-md-7"}, 
+                                    React.createElement("div", {className: "row"}, 
+                                        React.createElement("h2", null, React.createElement("a", {href:  movie.url},  movie.name))
+                                    ), 
+                                    React.createElement("div", {className: "row"}, 
+                                      React.createElement("ul", null, 
+                                        React.createElement("li", null, React.createElement("b", null, "Жанр: "),  movie.genre), 
+                                        React.createElement("li", null, React.createElement("b", null, "Оценка/Голосовало: "),  movie.rating, React.createElement("b", null, "/"),  movie.voted)
+                                      )
+                                    ), 
+                                    React.createElement("h3", null, React.createElement("a", {href:  movie.url},  movie.title)), 
+                                    React.createElement("p", null,  movie.description), 
+                                    React.createElement("p", null, React.createElement("b", null, "Описание:")), 
+                                    React.createElement("p", null,  movie.text)
+                                )
+                            )
+                        )
+                    }) 
+                )
+            )
+        )
+    }
+});
+
+var movies = [];
+
+ReactDOM.render(
+    React.createElement(MoviesList, {items: movies}), document.getElementById("main_list")
+);
 
 },{}]},{},[1]);
--- static/jsx/main.js
+++ static/jsx/main.js
@@ -1,12 +1,78 @@
 var socket = io.connect('http://' + document.domain + ':' + location.port);
-    // socket.on('connect', function() {
-    //     socket.emit('my event', {data: 'I\'m connected!'});
-    // });
-    socket.on('movie_loaded', function (movie) {
-        console.log(movie);
-        var d = document.getElementById('movies_row');
-        var stub = document.getElementById('stub');
-        if(stub){
-            d.removeChild(stub);
-        }
-    });
+var MoviesList = React.createClass({
+    getInitialState() {
+        return {
+            movies: [],
+            showSpinner: false
+        };
+    },
+
+    componentDidMount(){
+        socket.on('init', this._initialize);
+        socket.on('movie_loaded', this._load_movie);
+    },
+
+    _initialize(data){
+        var {movies, show} = data;
+        this.setState({movies, showSpinner: show});
+    },
+
+    _load_movie(movie){
+        var movies = this.state.movies;
+        movies.push(movie.data);
+        this.setState({movies, showSpinner: true});
+    },
+
+    render() {
+        var movies = this.state.movies;
+        var showSpinner = this.state.showSpinner;
+        return(
+            <div>
+                <div className="row">
+                    <div className={showSpinner ? 'loader' : 'loader hidden'}></div>
+                </div>
+                <ul>
+                    { movies.map(function (movie) {
+                        return <li>
+                            <div className="movie_class col-md-12 row">
+                                <div className="col-md-4">
+                                    <div className="moviediv">
+                                        <a href={ movie.url }>
+                                            <img src={ movie.image } className="img-rounded movie_image" alt={ movie.title }/>
+                                        </a>
+                                    </div>
+                                    <ul>
+                                        <li><b>Режиссер: </b>{ movie.director }</li>
+                                        <li><b>Продолжительность: </b>{ movie.duration }</li>
+                                        <li><b>Дата релиза: </b>{ movie.release }</li>
+                                    </ul>
+                                </div>
+                                <div className="col-md-7">
+                                    <div className="row">
+                                        <h2><a href={ movie.url }>{ movie.name }</a></h2>
+                                    </div>
+                                    <div className="row">
+                                      <ul>
+                                        <li><b>Жанр: </b>{ movie.genre }</li>
+                                        <li><b>Оценка/Голосовало: </b>{ movie.rating }<b>/</b>{ movie.voted }</li>
+                                      </ul>
+                                    </div>
+                                    <h3><a href={ movie.url }>{ movie.title }</a></h3>
+                                    <p>{ movie.description }</p>
+                                    <p><b>Описание:</b></p>
+                                    <p>{ movie.text }</p>
+                                </div>
+                            </div>
+                        </li>
+                    }) }
+                </ul>
+            </div>
+        )
+    }
+});
+
+var movies = [];
+
+ReactDOM.render(
+    <MoviesList items={movies}/>, document.getElementById("main_list")
+);
