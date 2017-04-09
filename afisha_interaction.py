754c2f3e961137b3a231b9931fb04c9ff312f038
rndviktor2devman <7irb291wqkmu@mail.ru>
2017-04-09 23:52:45 +0300

    rename variable

--- afisha_interaction.py
+++ afisha_interaction.py
@@ -18,32 +18,32 @@ def json_data(page):
     return json.loads(soup.find('script', {'type': 'application/ld+json'}).text)
 
 
-def parse_movie_data(raw_data):
+def parse_movie_data(movie_page):
     movie = {}
-    soup = BS(raw_data, "lxml")
-    raw_data = json.loads(soup.find('script', {'type': 'application/ld+json'}).text)
-    movie['director'] = raw_data.get('director', {'name': NO_INFO})['name']
-    movie['text'] = raw_data.get('text', NO_INFO)
-    movie['description'] = raw_data.get('description', NO_INFO)
-    movie['genre'] = raw_data.get('genre')
-    movie['name'] = raw_data.get('name', NO_INFO)
-    format_duration = raw_data.get('duration', {'name': NO_INFO})['name']
+    soup = BS(movie_page, "lxml")
+    movie_page = json.loads(soup.find('script', {'type': 'application/ld+json'}).text)
+    movie['director'] = movie_page.get('director', {'name': NO_INFO})['name']
+    movie['text'] = movie_page.get('text', NO_INFO)
+    movie['description'] = movie_page.get('description', NO_INFO)
+    movie['genre'] = movie_page.get('genre')
+    movie['name'] = movie_page.get('name', NO_INFO)
+    format_duration = movie_page.get('duration', {'name': NO_INFO})['name']
     format_duration = re.sub(r'PT(\d+)H(\d+)M', lambda m: str(int(m.group(1)) * 60 + int(m.group(2))),
                              format_duration)
     if format_duration != NO_INFO:
         format_duration += ' мин.'
     movie['duration'] = format_duration
-    release = raw_data.get('datePublished')
+    release = movie_page.get('datePublished')
     if release is None:
         year = NO_INFO
     else:
         release = release[0:release.find('T')]
         year = dt.datetime.strptime(release, "%Y-%M-%d").date().strftime("%d.%M.%Y")
     movie['release'] = year
-    movie['voted'] = raw_data.get('aggregateRating', {'ratingCount': 0})['ratingCount']
-    movie['rating'] = raw_data.get('aggregateRating', {'ratingValue': 0})['ratingValue']
-    movie['url'] = raw_data.get('url')
-    img_url = raw_data.get('image')
+    movie['voted'] = movie_page.get('aggregateRating', {'ratingCount': 0})['ratingCount']
+    movie['rating'] = movie_page.get('aggregateRating', {'ratingValue': 0})['ratingValue']
+    movie['url'] = movie_page.get('url')
+    img_url = movie_page.get('image')
     if img_url is None:
         img_url = STATIC_IMAGE
     movie['image'] = img_url
